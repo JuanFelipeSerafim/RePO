4c18e4dc95e4817660680d07d0aa39713bc1a772
Juan Felipe Serafim dos Santos <jfss@cin.ufpe.br>
2025-12-01 01:53:01 -0300

    links dos datasets pelo google drive

--- data/novoDataset/extract_new_dataset.sh
+++ data/novoDataset/extract_new_dataset.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+pip install gdown
+#baixando os datasets
+    #MSSQL.zip
+gdown 19KE1ubNI716Ew_n1uOzoTL1-z9JhvLA-
+    #UDP.zip
+gdown 1G_4VAIvBKFZIww1brrTuIMrk1S958SCC
+    #UDPLag.zip
+gdown 1VAullxnbXPRtKlwDwFIq-j9rUaY5JFbK
+    #NetBios.zip
+gdown 1g0VaFCwFXvCBTpH_1ZiqEE3FhVcEGPdp
+    #LDAP.zip
+gdown 1qO5q4--FLyng_Qes-1rM2hnbbq1REYS1
+    #Portmap.zip
+gdown 1qrSz8f_ZXpTucUM102zo6sbmheptBuiz
+    #Syn.zip
+gdown 1xDgIVdKIn71h0ruDYd0dPJdvW5N56Erj
 
 
 unzip LDAP.zip
